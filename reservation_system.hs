b453a3870ff55b93c6a1b329ed2dd7b9bf03a409
Fabian Ehrentraud <e0725639@lrcng.matrix.tuwien.ac.at>
2011-03-31 14:01:15 +0200

    beginning with HXT example

---
+++ reservation_system.hs
@@ -0,0 +1,34 @@
+--Fabian Ehrentraud, Bernhard Urban, 2011
+
+{- TODO
+
+	try out HXT example from http://www.haskell.org/haskellwiki/HXT
+		which just copies an XML file
+		
+	build a text-ui wit IO monad for user interaction
+
+-}
+
+module Main
+where
+ 
+import Text.XML.HXT.Core
+import Text.XML.HXT.Curl -- use libcurl for HTTP access
+						 -- only necessary when reading http://...
+ 
+import System.Environment
+ 
+main :: IO ()
+main
+	= do
+		[src, dst] <- getArgs
+		runX ( readDocument [withValidate no
+                            ,withCurl []
+                            ] src
+		>>>
+		writeDocument [withIndent yes
+		              ,withOutputEncoding isoLatin1
+		              ] dst
+		)
+		return ()
+
