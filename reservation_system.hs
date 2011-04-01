4e3b6bef07f5b38a7c5258d1f57659cc2b30ade4
fabb <milkservice@gmail.com>
2011-04-01 15:43:18 +0200

    this one actually compiles and works

--- reservation_system.hs
+++ reservation_system.hs
@@ -13,8 +13,7 @@ module Main
 where
  
 import Text.XML.HXT.Core
-import Text.XML.HXT.Curl -- use libcurl for HTTP access
-						 -- only necessary when reading http://...
+--import Text.XML.HXT.Curl -- use libcurl for HTTP access, only necessary when reading http://...
  
 import System.Environment
  
@@ -23,12 +22,11 @@ main
 	= do
 		[src, dst] <- getArgs
 		runX ( readDocument [withValidate no
-                            ,withCurl []
                             ] src
-		>>>
-		writeDocument [withIndent yes
-		              ,withOutputEncoding isoLatin1
-		              ] dst
-		)
+		       >>>
+		       writeDocument [withIndent yes
+		                     ,withOutputEncoding utf8
+		                     ] dst
+		     )
 		return ()
 
