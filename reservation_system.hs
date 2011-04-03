445602b7b990d5c8ca31332b8bd0ee3496838d8e
fabb <milkservice@gmail.com>
2011-04-03 18:36:46 +0200

    read/write data from xml pickles, anyone?

--- reservation_system.hs
+++ reservation_system.hs
@@ -18,112 +18,117 @@ import Text.XML.HXT.Core
 import System.Environment
 
 
+{---------- Globals ----------}
+xmlFilename = "reservations.xml" :: String
+
+
+{---------- Types ----------}
+type IssuedReservations = Integer
+type XMLData = (IssuedReservations, [RItem])
+type ApplicationData = (IssuedReservations, ReservationZipper)
+
 {---------- Zipper Type ----------}
 --efficiently navigable data structure for representing train reservations
 --it's in fact just a list zipper
-type ReservationZipper = ([RItem],[RCrumb]) --TODO use RCrumb?
+type ReservationZipper = ([RItem],[RCrumb])
 
 type ReservationNumber = Integer
-type GroupReservationData = [Integer] --TODO split into the single items
-type IndividualReservationData = [Integer] --TODO split into the single items
+type FromStation = Integer
+type ToStation = Integer
+type Train = Integer
+type TrainCar = Integer
+type SeatCount = Integer
+type SeatNumber = Integer
+
+type GroupReservationData = (FromStation, ToStation, Train, TrainCar, SeatCount)
+type IndividualReservationData = (FromStation, ToStation, Train, TrainCar, SeatNumber)
+
 data RItem = GroupReservation ReservationNumber GroupReservationData
              | IndividualReservation ReservationNumber GroupReservationData deriving (Show)
 type RCrumb = RItem 
 
-
-testZipper = ([GroupReservation 1 [1,2], GroupReservation 2 [2,3], IndividualReservation 3 [3,4], IndividualReservation 4 [4,5]],[])
 
 
 {---------- Main ----------} 
 main :: IO ()
 main = do
 		--[src, dst] <- getArgs
-		xmlFilename <- return "reservations.xml"
+		xmlFilename <- return xmlFilename
 		
 		printWelcome
 		
-		zipper <- loadData xmlFilename
+		d@(icount, zipper) <- loadData xmlFilename
 		
-		zipper <- return testZipper --TODO test
-		
+		putStrLn $ show d --TODO test
+
 		printMenu
 		
-		zipper <- mainloop zipper xmlFilename
+		d@(icount, zipper) <- mainloop d xmlFilename
 		
-		writeData zipper xmlFilename
+		writeData (icount, zipper) xmlFilename
 		
-		putStrLn $ show zipper --TODO test
+		putStrLn $ show d --TODO test
 		
 		printGoodbye
-		
-		{-
-		runX ( readDocument [withValidate no
-                            ] xmlFilename --beware if it not yet exists
-		       >>>
-		       writeDocument [withIndent yes
-		                     ,withOutputEncoding utf8
-		                     ] xmlFilename
-		     )
-		-}
 		
 		return ()
 
 
 --reads in user input and processes wanted changes on reservations
 --returns the changed data structure when finished TODO does it?
-mainloop :: ReservationZipper -> String -> IO ReservationZipper
-mainloop zipper xmlFilename = do
+mainloop :: ApplicationData -> String -> IO ApplicationData
+mainloop d@(icount, zipper) xmlFilename = do
 		
 		choice <- getLine
 		--putStr choice
 
 		--TODO process requests
 		--TODO is it stupid to change zipper like this - especially for non-changing functions?
-		zipper <- case choice of
+		d@(icount, zipper) <- case choice of
 			"a" -> do -- New individual reservation, needs as input FROM, TO, TRAIN, CAR, COUNT
 					printDummy choice
 					--return changedZipper
-					return zipper
+					return d
 					
 			"s" -> do -- New group reservation, needs as input FROM, TO, TRAIN, CAR, SEAT
 					printDummy choice
 					--return changedZipper
-					return zipper
+					return d
 					
 			"y" -> do -- Delete reservation, needs as input RESERVATIONNUMBER
 					printDummy choice
 					changedZipper <- return $ removeFirst zipper --TODO test
-					return changedZipper
+					return (icount, changedZipper)
 		
 			"r" -> do -- Show train stations, trains, train cars, and seats, needs no input
 					printDummy choice
-					return zipper
+					return d
 					
 			"d" -> do -- Show group reservations, needs as input TRAIN, CAR
 					printDummy choice
-					return zipper
+					return d
 					
 			"f" -> do -- Show free seat count, needs as input TRAIN, CAR, FROM, TO
 					printDummy choice
-					return zipper		
+					return d
 					
 			"g" -> do -- Show individual reservations, needs as input TRAIN, CAR, SEAT
 					printDummy choice
-					return zipper
+					return d
 					
 			"q" -> do
 					--TODO could I quit from here when the result is assigned to zipper with <- ?
 					printDummy choice
-					return zipper
+					return d
 			
 			_   -> do
 					putStrLn $ "Wrong input of '" ++ choice ++ "', please choose again."
-					return zipper
+					return d
 
 		if choice == "q"
-			then return zipper
+			then return d
 			else do
-				mainloop zipper xmlFilename
+				mainloop d xmlFilename
 
 
 
@@ -142,7 +147,7 @@ printGoodbye = putStrLn "\nWe wish you a nice day."
 
 --prints the available main menu operations	
 printMenu :: IO ()
-printMenu = putStrLn "Your Options:\n\
+printMenu = putStrLn "\nYour Options:\n\
                      \ (a) New individual reservation\n\
                      \ (s) New group reservation\n\
                      \ (y) Delete reservation\n\
@@ -164,26 +169,129 @@ printMenu = putStrLn "Your Options:\n\
 
 {---------- XML Handling ----------}
 --write the zipper data to xml
-writeData :: ReservationZipper -> String -> IO ()
-writeData zipper xmlFilename = do
-	--TODO write to file
-	--writeDocument [withIndent yes, withOutputEncoding utf8] xmlFilename
-	--putStrLn "Trying to write data to " ++ xmlFilename
+writeData :: ApplicationData -> String -> IO ()
+writeData (icount, zipper) xmlFilename = do
+	putStrLn $ "Trying to write data to " ++ xmlFilename
+	res <- return $ unpackRZipper zipper
+	runX
+			( constA (icount, res)
+			>>>
+			xpickleDocument xpReservationData
+				[ withIndent yes
+				, withOutputEncoding utf8
+				] xmlFilename
+            )
 	return ()
 
 
 --loads data from the given xml and prints status
 --TODO return zipper
-loadData :: String -> IO ReservationZipper
+loadData :: String -> IO ApplicationData
 loadData xmlFilename = do
-	--putStrLn $ "Trying to load reservation data from " ++ xmlFilename
-	--TODO load xml into zipper data structure
+	putStrLn $ "Trying to load reservation data from " ++ xmlFilename
 	--readDocument [withValidate no] xmlFilename --beware if it not yet exists
 	--if xml does not yet exist, say so and initialize zipper with default values (issued reservations number = 0)
-	return ([],[]) --ReservationZipper
-	
+	r@[(c, z)] <- runX
+			( xunpickleDocument xpReservationData
+				[ withRemoveWS yes   -- remove redundant whitespace
+				, withValidate no    -- don't validate source
+				] xmlFilename
+            )
+	if null r
+		then return (0, makeRZipper []) --FIXME pattern match failure at [(c, z)] and xunpickleDocument will return a fatal error
+		else return (c, makeRZipper z)
+
+{- test
+testReadXml xmlFilename = runX
+			( xunpickleDocument xpReservationData
+				[ withRemoveWS yes   -- remove redundant whitespace
+				, withTrace 1
+				, withValidate no    -- don't validate source
+				] xmlFilename
+            )
+
+testReadWriteXml xmlFilename = runX
+			( xunpickleDocument xpReservationData
+				[ withRemoveWS yes   -- remove redundant whitespace
+				, withTrace 1
+				, withValidate no    -- don't validate source
+				] xmlFilename
+			>>>
+			xpickleDocument xpReservationData
+				[ withIndent yes
+				] "test.xml"
+            )
+-}
+
+--start point for xml<->data transformations
+xpReservationData :: PU XMLData
+xpReservationData = xpElem "reservations" $ --xml root element
+		xpAddFixedAttr "xmlns:xsi" "http://www.w3.org/2001/XMLSchema-instance" $
+		xpAddFixedAttr "xsi:noNamespaceSchemaLocation" "reservations.xsd" $
+		xpPair
+			xpIssuedReservations
+			xpReservations
+ 
+--xpMissingRootElement :: PU XMLData
+--xpMissingRootElement = xpickle
+
+xpIssuedReservations :: PU IssuedReservations
+xpIssuedReservations = xpElem "issued_reservations" $
+		xpPrim
+
+{- another way of doing pickles is to just write "xpickle" and let a fitting one be chosen
+instance XmlPickler IssuedReservations where
+	xpickle = xpElem "issued_reservations" $
+		xpPrim
+-}
+
+xpReservations :: PU [RItem]
+xpReservations = 
+	--xpList xpGroupReservation
+	--xpList xpIndividualReservation
+	xpList $
+	xpAlt tag ps --doesn't this work easier? it's just a choice between xpIndividualReservation and xpGroupReservation
+	where
+	tag ( IndividualReservation _ _ ) = 0
+	tag ( GroupReservation _ _ ) = 1
+	ps = [ xpIndividualReservation
+		 , xpGroupReservation
+		 ]
+
+xpIndividualReservation :: PU RItem
+xpIndividualReservation = xpElem "individual_reservation" $
+		xpWrap ( \((resnum,from,to,train,car,seat)) -> IndividualReservation resnum (from, to, train, car, seat)
+		       , \ir@(IndividualReservation resnum (from, to, train, car, seat)) -> (resnum,from,to,train,car,seat)
+		       ) $
+		xp6Tuple (xpElem "reservation_number" xpPrim)
+				(xpElem "from" xpPrim)
+				(xpElem "to" xpPrim)
+				(xpElem "train" xpPrim)
+				(xpElem "car" xpPrim)
+				(xpElem "seat" xpPrim)
+
+xpGroupReservation :: PU RItem
+xpGroupReservation = xpElem "group_reservation" $
+		xpWrap ( \((resnum,from,to,train,car,count)) -> GroupReservation resnum (from, to, train, car, count)
+		       , \ir@(GroupReservation resnum (from, to, train, car, count)) -> (resnum,from,to,train,car,count)
+		       ) $
+		xp6Tuple (xpElem "reservation_number" xpPrim)
+				(xpElem "from" xpPrim)
+				(xpElem "to" xpPrim)
+				(xpElem "train" xpPrim)
+				(xpElem "car" xpPrim)
+				(xpElem "count" xpPrim)
+
 
 {---------- Zipper Stuff ----------}
+
+--creates a zipper from a list of reservations
+makeRZipper :: [RItem] -> ReservationZipper
+makeRZipper xs = (xs, [])
+
+--unpacks the zipper back to a list of reservations
+unpackRZipper :: ReservationZipper -> [RItem]
+unpackRZipper z = maybe [] fst $ goFirst z
 
 --forwards for one item
 goForward :: ReservationZipper -> Maybe ReservationZipper
