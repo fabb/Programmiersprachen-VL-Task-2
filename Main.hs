fabee4b31661f48b33f13d891854e9b50eb7eee3
fabb <milkservice@gmail.com>
2011-04-30 17:01:14 +0200

    Main module is needed when an executable should be generated with simple command in WinGHCi :! ghc --make "Main.hs"

---
+++ Main.hs
@@ -0,0 +1,10 @@
+--Train Reservation Management Application
+--Fabian Ehrentraud, 2011
+--Reviewer: Bernhard Urban
+
+module Main where
+
+import qualified ReservationSystem (main)
+
+main :: IO ()
+main = ReservationSystem.main
